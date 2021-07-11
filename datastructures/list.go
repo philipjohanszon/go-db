0ec7b353ad2f806f65c2355b7a9ec2fc97c39829
Philip Johansson <philip.hlm@live.se>
2021-07-11 20:53:23 +0200

    updated list to be specific to strings and updated trimstart

--- datastructures/list.go
+++ datastructures/list.go
@@ -1,19 +1,19 @@
 package datastructures
 
 //has end node so that it wont need to be looped to
-type List struct {
-	Next *ListNode
-	End  *ListNode
+type StringList struct {
+	Next *StringListNode
+	End  *StringListNode
 }
 
-type ListNode struct {
-	Next  *ListNode
-	Value interface{}
+type StringListNode struct {
+	Next  *StringListNode
+	Value string
 }
 
 //Adds several nodes so that it wont loop several times to get to the end
-func (list *List) AddNodes(value interface{}) {
-	newNode := ListNode{Next: nil, Value: value}
+func (list *StringList) AddNodes(value string) {
+	newNode := StringListNode{Next: nil, Value: value}
 
 	//Sets the end node to the new node for the currently last node and then it sets it for the list variable
 	(*list.End).Next = &newNode
@@ -21,7 +21,7 @@ func (list *List) AddNodes(value interface{}) {
 
 }
 
-func (list *List) TrimStart(newStartNode *ListNode) {
+func (list *StringList) TrimStart(newStartNode *StringListNode) {
 	list.Next = newStartNode
 
 	if newStartNode == nil {
